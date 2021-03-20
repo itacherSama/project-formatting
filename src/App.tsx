385dc06fd677eedc37bbdf91b3da0ccfdfe87091
elivanov.denis <elivanov.denis@mediasoft.team>
2021-03-20 11:47:43 +0400

    fix LF and add SW

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import styles from './App.module.css';
+import React from "react";
+import styles from "./App.module.css";
 
 const App: React.FC = ({ children }) => {
   return (
     <div className={ styles.container }>
-      <div className={ styles.content }>
-        { children }
-      </div>
+      <div className={ styles.content }>{ children }</div>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
-import ReactDOM from 'react-dom';
-import history from './router/history';
-import router from './router/router';
+import ReactDOM from "react-dom";
+import history from "./router/history";
+import router from "./router/router";
+import * as serviceWorker from "./serviceWorkerRegistration";
 
-import './index.css';
+import "./index.css";
 
 const render = async (location: any) => {
   const element: any = await router.resolve(location);
-  ReactDOM.render(
-    element,
-    document.getElementById('root'),
-  );
+  ReactDOM.render(element, document.getElementById("root"));
 };
 
 render(history.location);
 history.listen(render);
+
+serviceWorker.register();
