dde71a58b2b2bcf51502c1ec66ae75b4524209af
Itacher <itacher@yandex.ru>
2022-07-08 09:03:12 +0400

    fix(localStorage): удалено условие по сохранению данных

--- src/services/localStorageService.ts
+++ src/services/localStorageService.ts
@@ -2,10 +2,8 @@ import { IInfoImg, PossibleStringType } from 'interfaces/interfaces';
 import { convertFilesInBase64Items } from './base64Service';
 
 export const saveDataInLocalStorage = <T>(localName: string, data: Array<T>) => {
-  if (data.length) {
-    const convertedDataToString = JSON.stringify(data);
-    localStorage.setItem(localName, convertedDataToString);
-  }
+  const convertedDataToString = JSON.stringify(data);
+  localStorage.setItem(localName, convertedDataToString);
 };
 
 export const convertAndSaveDataInLocalStorage = (localName: string, data: Array<IInfoImg>): void => {
